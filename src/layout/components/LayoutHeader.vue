28588d7072d53bb82960ac9d27a1b6f77b883f13
黄瑞 <rui.huang@fintecher.cn>
2023-11-09 15:35:37 +0800

    :bug:  修改页面布局，头部不显示菜单标签

--- src/layout/components/LayoutHeader.vue
+++ src/layout/components/LayoutHeader.vue
@@ -1,29 +1,11 @@
 <script lang="ts" setup>
-import { useRoute } from 'vue-router'
-import { ref, watch } from 'vue'
 import HeaderAction from './HeaderAction.vue'
-import workspaceRoutes from '@/router/routes/workspace.routes'
-
-const currentRoute = useRoute()
-
-const title = ref('')
-
-watch(() => currentRoute.path, () => {
-  const menuPath = currentRoute.meta?.menuPath
-  if (menuPath)
-    title.value = workspaceRoutes.find(x => x.path === menuPath)?.meta?.title ?? ''
-  else
-    title.value = currentRoute.meta?.title ?? ''
-}, { immediate: true })
 </script>
 
 <template>
   <div class="layout-header">
     <icon-park-outline-brain class="sys-log" />
     <span class="sys-name">特赛发</span>
-    <div class="sys-title">
-      {{ title }}
-    </div>
     <HeaderAction class="flex-1" />
   </div>
 </template>
@@ -40,12 +22,6 @@ watch(() => currentRoute.path, () => {
 
   .sys-name {
     @apply ml-2 text-2xl font-bold;
-  }
-
-  .sys-title {
-    color: @color-primary;
-    box-shadow: 0px 0px 6px 0px #F0F3F7, 0px 4px 4px 0px rgba(157, 113, 255, 0.08);
-    @apply ml-10 rounded-3xl px-3 py-1 text-sm;
   }
 }
 </style>
